0a0a7e6196cb3b0d5174f3817d3b896e9f9b17e7
Deepanshu Singh <125585104+singh-deepanshu-578@users.noreply.github.com>
2024-12-16 22:09:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,3 +31,15 @@ document.addEventListener('DOMContentLoaded', function() {
         fileInput.click();
     });
 });
+
+document.addEventListener('mousemove', (e) => {
+    const eyes = document.querySelectorAll('.eye');
+    eyes.forEach(eye => {
+        const rect = eye.getBoundingClientRect();
+        const x = (rect.left + rect.width / 2);
+        const y = (rect.top + rect.height / 2);
+        const rad = Math.atan2(e.pageX - x, e.pageY - y);
+        const rot = (rad * (180 / Math.PI) * -1) + 180;
+        eye.style.transform = `rotate(${rot}deg)`;
+    });
+});
